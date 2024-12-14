1ebbf2aa3cbf6b9037e89ff36ec0e2acd74665f0
Koziakov Hlib <hlibs.work@gmail.com>
2024-12-14 15:02:31 +0200

    Implemented all  error classes and cafe class

--- app/errors.py
+++ app/errors.py
@@ -3,15 +3,15 @@ class VaccineError(Exception):
 
 
 class NotVaccinatedError(VaccineError):
-    def __init__(self, message="Visitor is not vaccinated") -> None:
+    def __init__(self, message: str = "Visitor is not vaccinated") -> None:
         super().__init__(message)
 
 
 class OutdatedVaccineError(VaccineError):
-    def __init__(self, message="Visitor's vaccine has expired") -> None:
+    def __init__(self, message: str = "Visitor's vaccine has expired") -> None:
         super().__init__(message)
 
 
 class NotWearingMaskError(Exception):
-    def __init__(self, message="Visitor is not wearing a mask") -> None:
+    def __init__(self, message: str = "Visitor is not wearing a mask") -> None:
         super().__init__(message)
